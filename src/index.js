c57f94b58a813ac7485ae718d9092dd01ba56f92
wendylian <wendylian@tencent.com>
2022-01-13 15:11:15 +0800

    initial project gzhipin-client

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store'
+import Login from './containers/login/login'
+import Register from './containers/register/register'
+import Main from './containers/main/main'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+    <Provider store={store}>
+      <HashRouter>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route component={Main} />
+        </Switch>
+      </HashRouter>
+    </Provider>
+  ),
   document.getElementById('root')
 );
 
